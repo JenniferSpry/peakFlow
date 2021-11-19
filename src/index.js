95c8fad1def9df78b5d3e4d6feec2913fd832e8a
JenniferSpry <jennifer.spry@mindmatters.de>
2021-11-19 12:30:09 +0100

    add development utils

--- src/index.js
+++ src/index.js
@@ -1,15 +1,61 @@
-import _ from 'lodash';
-import Peaks from './data.csv';
+import * as d3 from "d3";
+import data from './data.csv';
+import "./style.css"; // hack to get the css file loaded
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  console.log(Peaks);
+console.log(data)
 
-  document.body.appendChild(component());
+const svgElement = document.createElement('svg');
+document.body.appendChild(svgElement);
+
+const margin = 60;
+const width = 1000 - 2 * margin;
+const height = 600 - 2 * margin;
+
+const svg = d3.select('svg');
+
+const chart = svg.append('g')
+    .attr('transform', `translate(${margin}, ${margin})`);
+
+// var yScale = d3.scaleLinear()
+//     .range ([height, 0])
+//     .domain([0, 100]);
+
+// chart.append('g')
+//     .call(d3.axisLeft(yScale));
+
+const xScale = d3.scaleBand()
+    .range([0, width])
+    .domain(data.map((s) => s.timestamp))
+    .padding(0.2)
+
+chart.append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale));
+
+// var svg = d3.select("svg"),
+//     margin = 200,
+//     width = svg.attr("width") - margin,
+//     height = svg.attr("height") - margin;
+
+// var xScale = d3.scaleBand().range ([0, width]).padding(0.4),
+//     yScale = d3.scaleLinear().range ([height, 0]);
+
+// var g = svg.append("g")
+//         .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+// xScale.domain(Test.map(function(d) { return d.year; }));
+// yScale.domain([0, d3.max(Test, function(d) { return d.value; })]);
+
+// g.append("g")
+// .attr("transform", "translate(0," + height + ")")
+// .call(d3.axisBottom(xScale));
+
+// g.append("g")
+// .call(d3.axisLeft(yScale).tickFormat(function(d){
+//     return "$" + d;
+// }).ticks(10))
+// .append("text")
+// .attr("y", 6)
+// .attr("dy", "0.71em")
+// .attr("text-anchor", "end")
+// .text("value");
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    clean: true, // empty dist folder
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
   },
   plugins: [
-    new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({ // create the index.html file from template
       title: 'Peak Flow',
+      hash: true, // cache busting
+      metaDesc: 'Peak Flow',
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body'
     }),
+    new MiniCssExtractPlugin()
   ],
   module: {
     rules: [
       {
+        loader: 'csv-loader', // load csv content into js bundle
         test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      }
+        options: {
+          dynamicTyping: true,
+          header: true,
+          skipEmptyLines: true
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader, // only loads stuff it finds in the index.js, will not just copy my style.css file
+          'css-loader'
+        ],
+      },
     ]
   }
 };
